4bbed1139efe17fff729f711a2ac3d29d6859183
Okechukwu Joshua Ogo <127107918+okaa247@users.noreply.github.com>
2023-05-11 00:18:52 +0100

    Update CSV_read_write_script.py

--- CSV_read_write_script.py
+++ CSV_read_write_script.py
@@ -1,7 +1,10 @@
-import csv
-
 # Python program to demonstrate
 # writing to CSV
+
+import os
+import sys
+import csv
+import pandas as pd
 	
 # field names
 fields = ['Name', 'Dept', 'Course', 'Scores']
@@ -16,17 +19,19 @@ rows = [ ['Okechukwu', 'BCH', 'BCH302', '90'],
 
 	
 # writing to csv file
-with open('school_records.csv', 'w', newline = '') as csvfile:
+with open(os.path.join(sys.path[0], 'school_records.csv'), 'w', newline = '') as csvfile:
 	# creating a csv writer object
 	my_csvfile = csv.writer(csvfile)
 		
-	# writing the fields
+	# writing the data fields
 	my_csvfile.writerow(fields)
 		
 	# writing the data rows
-	my_csvfile.writerows(rows)
-	
-#Reading the csv file
-with open('school_records.csv', 'r') as my_csvfile:
-	read_file = my_csvfile.read()
+	for records in rows:
+		print(records)
+		my_csvfile.writerow(records)
+
+#Reading the csv file as school_records
+with open(os.path.join(sys.path[0], 'school_records.csv'),'r') as my_csvfile:
+	read_file = pd.read_csv(my_csvfile)
 	print(read_file)
